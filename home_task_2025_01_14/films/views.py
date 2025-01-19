0226f02b95168cae910c4c9b5d5f177942726683
LupinaIgor <igorlupina222@gmail.com>
2025-01-19 12:31:25 +0200

    Home_task 14.01.2025 part 1 completed

--- home_task_2025_01_14/films/views.py
+++ home_task_2025_01_14/films/views.py
@@ -1,3 +1,40 @@
+import requests
+from functools import wraps
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+BASE_URL = "https://swapi.dev/api"
+
+def fetch_data(endpoint):
+    """
+    Декоратор для виконання запитів до API.
+    """
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(request, *args, **kwargs):
+            url = f"{BASE_URL}/{endpoint}"
+            if 'id' in kwargs:
+                url = f"{url}/{kwargs['id']}/"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                return view_func(request, data, *args, **kwargs)
+            else:
+                return JsonResponse({'error': 'Failed to fetch data from API'}, status=response.status_code)
+        return wrapper
+    return decorator
+
+@fetch_data("films")
+def films_list(request, data):
+    """
+    Представлення для відображення списка фильмів.
+    """
+    films = data.get('results', [])
+    return render(request, 'films/films_list.html', {'films': films})
+
+@fetch_data("films")
+def film_detail(request, data, id):
+    """
+    Представлення для відображення информації про конкретний фільм.
+    """
+    return render(request, 'films/film_detail.html', {'film': data})

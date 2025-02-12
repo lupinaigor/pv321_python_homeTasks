be3627ab8dedaed64bc86e0f394ec4fff0facc10
LupinaIgor <igorlupina222@gmail.com>
2025-02-12 14:29:30 +0200

    Home_task 31.01.2025 BasketballPlayers completed change1

--- home_task_2025_01_31/BasketballAPI/players/models.py
+++ home_task_2025_01_31/BasketballAPI/players/models.py
@@ -1,8 +1,8 @@
-from django.db import models
-
-class BasketballPlayer(models.Model):
-    name = models.CharField(max_length=255)
-    height = models.IntegerField()
-
-    def __str__(self):
-        return self.name
+# from django.db import models
+#
+# class BasketballPlayer(models.Model):
+#     name = models.CharField(max_length=255)
+#     height = models.IntegerField()
+#
+#     def __str__(self):
+#         return self.name
